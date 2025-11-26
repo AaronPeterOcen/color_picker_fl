99b549c40fd267469797a004a62ef7030c45791d
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-11-26 11:44:12 +0300

    colorcard widget update

---
+++ lib/widgets/color_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ColorCard extends StatelessWidget {
+  final Color color;
+  final bool isLocked;
+  final VoidCallback onTap;
+  final bool showHex;
+
+  const ColorCard({
+    Key? key,
+    required this.color,
+    required this.isLocked,
+    required this.onTap,
+    this.showHex = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: color,
+        child: Stack(
+          children: [
+            if (showHex)
+              Positioned(
+                bottom: 8,
+                left: 8,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                  decoration: BoxDecoration(
+                    color: Colors.black45,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Text(
+                    _getHexCode(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ),
+            Positioned(
+              top: 8,
+              right: 8,
+              child: Container(
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: Colors.black45,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  isLocked ? Icons.lock : Icons.lock_open,
+                  color: Colors.white,
+                  size: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _getHexCode() {
+    // ignore: deprecated_member_use
+    return '#${color.red.toRadixString(16).padLeft(2, '0')}${color.green.toRadixString(16).padLeft(2, '0')}${color.blue.toRadixString(16).padLeft(2, '0')}'
+        .toUpperCase();
+  }
+}
